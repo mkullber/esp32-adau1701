e77fd466c2971186073738fc2fbcd2a594f92a90
Matti Kullberg <matti.kullberg@iki.fi>
2021-10-22 17:24:08 +0300

    Indent

--- src/main.cpp
+++ src/main.cpp
@@ -57,250 +57,250 @@ void handleSerial2Line();
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial2.begin(115200);
+    Serial.begin(115200);
+    Serial2.begin(115200);
 
-  if (!SPIFFS.begin(true))
-  {
-    Serial.println("An Error has occurred while mounting SPIFFS");
-  }
+    if (!SPIFFS.begin(true))
+    {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+    }
 
-  iotWebConf.setStatusPin(STATUS_PIN);
-  iotWebConf.setConfigPin(CONFIG_PIN);
-  iotWebConf.addSystemParameter(&switchAddressParam);
-  // iotWebConf.setConfigSavedCallback(&configSaved);
-  iotWebConf.setFormValidator(&formValidator);
-  iotWebConf.setWifiConnectionCallback(&wifiConnected);
-  char updatePath[32];
-  httpUpdater.setup(&server, updatePath);
-  iotWebConf.setupUpdateServer(
-      [](const char *updatePath)
-      { httpUpdater.setup(&server, updatePath); },
-      [](const char *userName, char *password)
-      { httpUpdater.updateCredentials(userName, password); });
+    iotWebConf.setStatusPin(STATUS_PIN);
+    iotWebConf.setConfigPin(CONFIG_PIN);
+    iotWebConf.addSystemParameter(&switchAddressParam);
+    // iotWebConf.setConfigSavedCallback(&configSaved);
+    iotWebConf.setFormValidator(&formValidator);
+    iotWebConf.setWifiConnectionCallback(&wifiConnected);
+    char updatePath[32];
+    httpUpdater.setup(&server, updatePath);
+    iotWebConf.setupUpdateServer(
+        [](const char *updatePath)
+        { httpUpdater.setup(&server, updatePath); },
+        [](const char *userName, char *password)
+        { httpUpdater.updateCredentials(userName, password); });
 
-  iotWebConf.skipApStartup();
+    iotWebConf.skipApStartup();
 
-  // -- Initializing the configuration.
-  boolean validConfig = iotWebConf.init();
-  if (!validConfig)
-  {
-    // mqttServerValue[0] = '\0';
-  }
+    // -- Initializing the configuration.
+    boolean validConfig = iotWebConf.init();
+    if (!validConfig)
+    {
+        // mqttServerValue[0] = '\0';
+    }
 
-  // -- Set up required URL handlers on the web server.
-  server.serveStatic("/", SPIFFS, "/index.html");
-  server.on("/config", []
-            { iotWebConf.handleConfig(); });
-  server.onNotFound([]()
-                    {
-                      if (!handleFileRead(server.uri()))
-                        iotWebConf.handleNotFound();
-                    });
+    // -- Set up required URL handlers on the web server.
+    server.serveStatic("/", SPIFFS, "/index.html");
+    server.on("/config", []
+              { iotWebConf.handleConfig(); });
+    server.onNotFound([]()
+                      {
+                          if (!handleFileRead(server.uri()))
+                              iotWebConf.handleNotFound();
+                      });
 
-  server.on("/is_connected", []
-            {
-              server.sendHeader("Access-Control-Allow-Origin", "*");
-              server.send(200, "application/json", adau.isConnectedJSON());
-            });
-  server.on("/connect", []
-            {
-              adau.connect();
-              server.sendHeader("Access-Control-Allow-Origin", "*");
-              server.send(200, "application/json", adau.isConnectedJSON());
-            });
-  server.on("/disconnect", []
-            {
-              adau.disconnect();
-              server.sendHeader("Access-Control-Allow-Origin", "*");
-              server.send(200, "application/json", adau.isConnectedJSON());
-            });
-  server.on("/get_values", handleGetValues);
-  server.on("/switch", handleSwitch);
+    server.on("/is_connected", []
+              {
+                  server.sendHeader("Access-Control-Allow-Origin", "*");
+                  server.send(200, "application/json", adau.isConnectedJSON());
+              });
+    server.on("/connect", []
+              {
+                  adau.connect();
+                  server.sendHeader("Access-Control-Allow-Origin", "*");
+                  server.send(200, "application/json", adau.isConnectedJSON());
+              });
+    server.on("/disconnect", []
+              {
+                  adau.disconnect();
+                  server.sendHeader("Access-Control-Allow-Origin", "*");
+                  server.send(200, "application/json", adau.isConnectedJSON());
+              });
+    server.on("/get_values", handleGetValues);
+    server.on("/switch", handleSwitch);
 
-  server.on("/list", handleFileList);
-  server.on("/edit", HTTP_PUT, handleFileCreate);
-  server.on("/edit", HTTP_DELETE, handleFileDelete);
-  server.on(
-      "/edit", HTTP_POST, []()
-      {
-        server.sendHeader("Access-Control-Allow-Origin", "*");
-        server.send(200, "text/plain", "");
-      },
-      handleFileUpload);
-  server.on("/upload", handleMinimalUpload);
+    server.on("/list", handleFileList);
+    server.on("/edit", HTTP_PUT, handleFileCreate);
+    server.on("/edit", HTTP_DELETE, handleFileDelete);
+    server.on(
+        "/edit", HTTP_POST, []()
+        {
+            server.sendHeader("Access-Control-Allow-Origin", "*");
+            server.send(200, "text/plain", "");
+        },
+        handleFileUpload);
+    server.on("/upload", handleMinimalUpload);
 
-  server.on("/edit", HTTP_GET, [&]()
-            {
-              if (!handleFileRead(server.uri()))
-                iotWebConf.handleNotFound();
-            });
+    server.on("/edit", HTTP_GET, [&]()
+              {
+                  if (!handleFileRead(server.uri()))
+                      iotWebConf.handleNotFound();
+              });
 
-  ArduinoOTA
-      .onStart([]()
-               {
-                 String type;
-                 if (ArduinoOTA.getCommand() == U_FLASH)
-                   type = "sketch";
-                 else // U_SPIFFS
-                   type = "filesystem";
+    ArduinoOTA
+        .onStart([]()
+                 {
+                     String type;
+                     if (ArduinoOTA.getCommand() == U_FLASH)
+                         type = "sketch";
+                     else // U_SPIFFS
+                         type = "filesystem";
 
-                 // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-                 Serial.println("Start updating " + type);
-               })
-      .onEnd([]()
-             { Serial.println("\nEnd"); })
-      .onProgress([](unsigned int progress, unsigned int total)
-                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
-      .onError([](ota_error_t error)
-               {
-                 Serial.printf("Error[%u]: ", error);
-                 if (error == OTA_AUTH_ERROR)
-                   Serial.println("Auth Failed");
-                 else if (error == OTA_BEGIN_ERROR)
-                   Serial.println("Begin Failed");
-                 else if (error == OTA_CONNECT_ERROR)
-                   Serial.println("Connect Failed");
-                 else if (error == OTA_RECEIVE_ERROR)
-                   Serial.println("Receive Failed");
-                 else if (error == OTA_END_ERROR)
-                   Serial.println("End Failed");
-               });
+                     // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+                     Serial.println("Start updating " + type);
+                 })
+        .onEnd([]()
+               { Serial.println("\nEnd"); })
+        .onProgress([](unsigned int progress, unsigned int total)
+                    { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+        .onError([](ota_error_t error)
+                 {
+                     Serial.printf("Error[%u]: ", error);
+                     if (error == OTA_AUTH_ERROR)
+                         Serial.println("Auth Failed");
+                     else if (error == OTA_BEGIN_ERROR)
+                         Serial.println("Begin Failed");
+                     else if (error == OTA_CONNECT_ERROR)
+                         Serial.println("Connect Failed");
+                     else if (error == OTA_RECEIVE_ERROR)
+                         Serial.println("Receive Failed");
+                     else if (error == OTA_END_ERROR)
+                         Serial.println("End Failed");
+                 });
 
-  if (!adau.init(0x68))
-  {
-    Serial.println("ADAU1701.init() fail");
-  }
-  // adau.i2cScan();
+    if (!adau.init(0x68))
+    {
+        Serial.println("ADAU1701.init() fail");
+    }
+    // adau.i2cScan();
 }
 
 void loop()
 {
-  iotWebConf.doLoop();
-  if (isWifiConnected)
-  {
-    webSocket.loop();
-    ArduinoOTA.handle();
-  }
-
-  if (needReset)
-  {
-    Serial.println("Rebooting after 1 second.");
-    iotWebConf.delay(1000);
-    ESP.restart();
-  }
-
-  while (Serial2.available())
-  {
-    char ch;
-    int nBytes;
-    nBytes = Serial2.read(&ch, 1);
-    if (nBytes > 0)
+    iotWebConf.doLoop();
+    if (isWifiConnected)
     {
-      if (ch == '\n')
-      {
-        serial2Buf[serial2BufPtr] = '\0';
-        handleSerial2Line();
-        serial2BufPtr = 0;
-      }
-      else if (ch != '\r')
-      {
-        serial2Buf[serial2BufPtr++] = ch;
-        if (serial2BufPtr >= sizeof(serial2Buf))
-        {
-          // buffer overflow
-          serial2BufPtr = 0;
-        }
-      }
+        webSocket.loop();
+        ArduinoOTA.handle();
     }
-  }
+
+    if (needReset)
+    {
+        Serial.println("Rebooting after 1 second.");
+        iotWebConf.delay(1000);
+        ESP.restart();
+    }
+
+    while (Serial2.available())
+    {
+        char ch;
+        int nBytes;
+        nBytes = Serial2.read(&ch, 1);
+        if (nBytes > 0)
+        {
+            if (ch == '\n')
+            {
+                serial2Buf[serial2BufPtr] = '\0';
+                handleSerial2Line();
+                serial2BufPtr = 0;
+            }
+            else if (ch != '\r')
+            {
+                serial2Buf[serial2BufPtr++] = ch;
+                if (serial2BufPtr >= sizeof(serial2Buf))
+                {
+                    // buffer overflow
+                    serial2BufPtr = 0;
+                }
+            }
+        }
+    }
 }
 
 void wifiConnected()
 {
-  webSocket.begin();
-  webSocket.onEvent(webSocketEvent);
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
 
-  ArduinoOTA.begin();
+    ArduinoOTA.begin();
 
-  isWifiConnected = true;
+    isWifiConnected = true;
 }
 
 void handleSerial2Line()
 {
-  char buf[32];
-  if (strncmp(serial2Buf, "VOL = ", 6) == 0)
-  {
-    int16_t volume;
-    if (sscanf(serial2Buf + 6, "%hi", &volume) == 1)
+    char buf[32];
+    if (strncmp(serial2Buf, "VOL = ", 6) == 0)
     {
-      // macOS volume adjustment: sqrt( linear 0..1 )
-      uint32_t volumePositive = volume + 24576;
-      uint32_t volumeScaled = volumePositive * volumePositive / 72; // 24576^2 / 0x00800000 = 72
-      Serial.printf("Volume: %i -> %u (0x%08X)\n", volume, volumeScaled, volumeScaled);
-      adau.setMasterVolume(volumeScaled);
-      if (isWifiConnected)
-      {
-        snprintf(buf, sizeof(buf) - 1, "{\"volume\": %u}", volumeScaled);
-        webSocket.broadcastTXT(buf);
-      }
+        int16_t volume;
+        if (sscanf(serial2Buf + 6, "%hi", &volume) == 1)
+        {
+            // macOS volume adjustment: sqrt( linear 0..1 )
+            uint32_t volumePositive = volume + 24576;
+            uint32_t volumeScaled = volumePositive * volumePositive / 72; // 24576^2 / 0x00800000 = 72
+            Serial.printf("Volume: %i -> %u (0x%08X)\n", volume, volumeScaled, volumeScaled);
+            adau.setMasterVolume(volumeScaled);
+            if (isWifiConnected)
+            {
+                snprintf(buf, sizeof(buf) - 1, "{\"volume\": %u}", volumeScaled);
+                webSocket.broadcastTXT(buf);
+            }
+        }
     }
-  }
-  else if (strncmp(serial2Buf, "MUTE = ", 7) == 0)
-  {
-    uint8_t muteNum;
-    boolean mute;
-    if (sscanf(serial2Buf + 7, "%hhu", &muteNum) == 1)
+    else if (strncmp(serial2Buf, "MUTE = ", 7) == 0)
     {
-      mute = muteNum == 1;
-      Serial.printf("Mute: %i\n", mute);
-      if (isWifiConnected)
-      {
-        snprintf(buf, sizeof(buf) - 1, "{\"mute\": %u}", mute);
-        webSocket.broadcastTXT(buf);
-      }
+        uint8_t muteNum;
+        boolean mute;
+        if (sscanf(serial2Buf + 7, "%hhu", &muteNum) == 1)
+        {
+            mute = muteNum == 1;
+            Serial.printf("Mute: %i\n", mute);
+            if (isWifiConnected)
+            {
+                snprintf(buf, sizeof(buf) - 1, "{\"mute\": %u}", mute);
+                webSocket.broadcastTXT(buf);
+            }
+        }
     }
-  }
-  else if (strncmp(serial2Buf, "PLAY = ", 7) == 0)
-  {
-    uint8_t playNum;
-    boolean play;
-    if (sscanf(serial2Buf + 7, "%hhu", &playNum) == 1)
+    else if (strncmp(serial2Buf, "PLAY = ", 7) == 0)
     {
-      play = playNum == 1;
-      Serial.printf("Play: %i\n", play);
-      if (isWifiConnected)
-      {
-        snprintf(buf, sizeof(buf) - 1, "{\"play\": %u}", play);
-        webSocket.broadcastTXT(buf);
-      }
-      // TODO: save play timestamp, turn switch on/off (off delay)
+        uint8_t playNum;
+        boolean play;
+        if (sscanf(serial2Buf + 7, "%hhu", &playNum) == 1)
+        {
+            play = playNum == 1;
+            Serial.printf("Play: %i\n", play);
+            if (isWifiConnected)
+            {
+                snprintf(buf, sizeof(buf) - 1, "{\"play\": %u}", play);
+                webSocket.broadcastTXT(buf);
+            }
+            // TODO: save play timestamp, turn switch on/off (off delay)
+        }
     }
-  }
-  else
-  {
-    Serial.print("Serial2: ");
-    Serial.println(serial2Buf);
-  }
+    else
+    {
+        Serial.print("Serial2: ");
+        Serial.println(serial2Buf);
+    }
 }
 
 void configSaved()
 {
-  Serial.println("Configuration was updated.");
-  needReset = true;
+    Serial.println("Configuration was updated.");
+    needReset = true;
 }
 
 bool formValidator(iotwebconf::WebRequestWrapper *webRequestWrapper)
 {
-  Serial.println("Validating form.");
-  boolean valid = true;
+    Serial.println("Validating form.");
+    boolean valid = true;
 
-  // int l = webRequestWrapper->arg(stringParam.getId()).length();
-  // if (l < 3)
-  // {
-  //   stringParam.errorMessage = "Please provide at least 3 characters for this test!";
-  //   valid = false;
-  // }
+    // int l = webRequestWrapper->arg(stringParam.getId()).length();
+    // if (l < 3)
+    // {
+    //   stringParam.errorMessage = "Please provide at least 3 characters for this test!";
+    //   valid = false;
+    // }
 
-  return valid;
+    return valid;
 }
--- src/request_handlers.h
+++ src/request_handlers.h
@@ -10,149 +10,149 @@ extern char switchAddressValue[STRING_LEN];
 
 void handleGetValues()
 {
-  server.sendHeader("Access-Control-Allow-Origin", "*");
-  // jsonDoc["value1"] = "foo";
-  // String valuesStr;
-  // serializeJson(jsonDoc, valuesStr);
-  // server.send(200, "application/json", valuesStr);
-  if (!adau.readValues())
-    Serial.println("adau.loadValues() fail");
-  server.send(200, "application/json", adau.valuesJSON());
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    // jsonDoc["value1"] = "foo";
+    // String valuesStr;
+    // serializeJson(jsonDoc, valuesStr);
+    // server.send(200, "application/json", valuesStr);
+    if (!adau.readValues())
+        Serial.println("adau.loadValues() fail");
+    server.send(200, "application/json", adau.valuesJSON());
 }
 
 bool tasmotaSwitch(String state, bool *switchStatus)
 {
-  bool status = false;
+    bool status = false;
 
-  String url = "http://" + String(switchAddressValue) + "/cm?cmnd=Power";
-  if (state != NULL && state.length() > 0)
-  {
-    url += "%20" + state;
-  }
-  HTTPClient http;
-  String result;
-  if (http.begin(url))
-  {
-    int httpStatus = http.GET();
-    status = httpStatus > 0;
-    if (httpStatus > 0)
+    String url = "http://" + String(switchAddressValue) + "/cm?cmnd=Power";
+    if (state != NULL && state.length() > 0)
     {
-      result = http.getString();
+        url += "%20" + state;
     }
-  }
-  http.end();
+    HTTPClient http;
+    String result;
+    if (http.begin(url))
+    {
+        int httpStatus = http.GET();
+        status = httpStatus > 0;
+        if (httpStatus > 0)
+        {
+            result = http.getString();
+        }
+    }
+    http.end();
 
-  if (status && switchStatus != NULL)
-  {
-    DynamicJsonDocument doc(32);
-    DeserializationError error = deserializeJson(doc, result);
-    if (error)
+    if (status && switchStatus != NULL)
     {
-      status = false;
+        DynamicJsonDocument doc(32);
+        DeserializationError error = deserializeJson(doc, result);
+        if (error)
+        {
+            status = false;
+        }
+        else
+        {
+            if (strcmp(doc["POWER"], "ON") == 0)
+            {
+                *switchStatus = true;
+            }
+            else
+            {
+                *switchStatus = false;
+            }
+        }
     }
-    else
-    {
-      if (strcmp(doc["POWER"], "ON") == 0)
-      {
-        *switchStatus = true;
-      }
-      else
-      {
-        *switchStatus = false;
-      }
-    }
-  }
 
-  return status;
+    return status;
 }
 
 void handleSwitch()
 {
-  String stateStr = String();
-  if (server.hasArg("state"))
-  {
-    stateStr = server.arg("state");
-  }
+    String stateStr = String();
+    if (server.hasArg("state"))
+    {
+        stateStr = server.arg("state");
+    }
 
-  bool state;
-  DynamicJsonDocument doc(32);
-  if (tasmotaSwitch(stateStr, &state))
-  {
-    doc["switch"] = state;
-  }
-  String resp;
-  serializeJson(doc, resp);
-  server.send(200, "text/plain", resp);
+    bool state;
+    DynamicJsonDocument doc(32);
+    if (tasmotaSwitch(stateStr, &state))
+    {
+        doc["switch"] = state;
+    }
+    String resp;
+    serializeJson(doc, resp);
+    server.send(200, "text/plain", resp);
 }
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
 {
-  bool status = false;
+    bool status = false;
 
-  switch (type)
-  {
-  case WStype_DISCONNECTED:
-    Serial.printf("[%u] Disconnected!\n", num);
-    break;
-  case WStype_CONNECTED:
-  {
-    IPAddress ip = webSocket.remoteIP(num);
-    Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-
-    // send message to client
-    webSocket.sendTXT(num, "WS connected");
-  }
-  break;
-  case WStype_TEXT:
-    Serial.printf("WS[%u] text: %s\n", num, payload);
-
-    if (strncmp((const char *)payload, "masterVolume: ", 14) == 0)
+    switch (type)
     {
-      uint32_t value;
-      if (sscanf((const char *)(payload + 14), "%u", &value) == 1)
-      {
-        Serial.printf("slider value: %u -> %08x\n", value, value);
-        status = adau.setMasterVolume(value);
-      }
-    }
-
-    if (strncmp((const char *)payload, "heartbeat", 9) != 0)
+    case WStype_DISCONNECTED:
+        Serial.printf("[%u] Disconnected!\n", num);
+        break;
+    case WStype_CONNECTED:
     {
-      webSocket.sendTXT(num, status ? "OK" : "FAIL");
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+
+        // send message to client
+        webSocket.sendTXT(num, "WS connected");
     }
+    break;
+    case WStype_TEXT:
+        Serial.printf("WS[%u] text: %s\n", num, payload);
 
-    // send data to all connected clients
-    // webSocket.broadcastTXT("message here");
-    break;
-  case WStype_BIN:
-    Serial.printf("WS[%u] binary (%u)\n", num, length);
-    hexdump(payload, length);
+        if (strncmp((const char *)payload, "masterVolume: ", 14) == 0)
+        {
+            uint32_t value;
+            if (sscanf((const char *)(payload + 14), "%u", &value) == 1)
+            {
+                Serial.printf("slider value: %u -> %08x\n", value, value);
+                status = adau.setMasterVolume(value);
+            }
+        }
 
-    // send message to client
-    // webSocket.sendBIN(num, payload, length);
-    break;
-  case WStype_PING:
-    Serial.printf("WS[%u] ping (%u)\n", num, length);
-    hexdump(payload, length);
-    break;
-  case WStype_PONG:
-  case WStype_ERROR:
-    Serial.println("ws error");
-    break;
-  case WStype_FRAGMENT_TEXT_START:
-  case WStype_FRAGMENT_BIN_START:
-  case WStype_FRAGMENT:
-  case WStype_FRAGMENT_FIN:
-    break;
-  }
+        if (strncmp((const char *)payload, "heartbeat", 9) != 0)
+        {
+            webSocket.sendTXT(num, status ? "OK" : "FAIL");
+        }
+
+        // send data to all connected clients
+        // webSocket.broadcastTXT("message here");
+        break;
+    case WStype_BIN:
+        Serial.printf("WS[%u] binary (%u)\n", num, length);
+        hexdump(payload, length);
+
+        // send message to client
+        // webSocket.sendBIN(num, payload, length);
+        break;
+    case WStype_PING:
+        Serial.printf("WS[%u] ping (%u)\n", num, length);
+        hexdump(payload, length);
+        break;
+    case WStype_PONG:
+    case WStype_ERROR:
+        Serial.println("ws error");
+        break;
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+        break;
+    }
 }
 
 void handleMinimalUpload()
 {
-  char temp[1500];
+    char temp[1500];
 
-  snprintf(temp, 1500,
-           "<!DOCTYPE html>\
+    snprintf(temp, 1500,
+             "<!DOCTYPE html>\
     <html>\
       <head>\
         <title>ESP8266 Upload</title>\
@@ -168,6 +168,6 @@ void handleMinimalUpload()
          </form>\
       </body>\
     </html>");
-  server.sendHeader("Access-Control-Allow-Origin", "*");
-  server.send(200, "text/html", temp);
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.send(200, "text/html", temp);
 }
