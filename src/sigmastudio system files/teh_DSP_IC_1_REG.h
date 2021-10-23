c6d4bd9dd0b757d0974ec655493a79cb7713c61f
Matti Kullberg <matti.kullberg@iki.fi>
2021-10-23 18:01:18 +0300

    Update Sigmastudio files

--- src/sigmastudio system files/teh_DSP_IC_1_REG.h
+++ src/sigmastudio system files/teh_DSP_IC_1_REG.h
@@ -1,7 +1,7 @@
 /*
  * File:           Z:\Sigmastudio teh DSP\system files\teh_DSP_IC_1_REG.h
  *
- * Created:        Friday, October 22, 2021 8:33:43 AM
+ * Created:        Saturday, October 23, 2021 1:04:58 PM
  * Description:    teh_DSP:IC 1 control register definitions.
  *
  * This software is distributed in the hope that it will be useful,
